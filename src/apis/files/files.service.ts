cb2c0e338dbca2a70428283df19405a1d74d0330
nwd0907 <nwd0907@newbizstart.com>
2023-02-19 08:37:42 +0900

    first commit

---
+++ src/apis/files/files.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { Storage } from '@google-cloud/storage';
+import { IFilesServiceUpload } from './interfaces/files-service.interface';
+
+@Injectable()
+export class FilesService {
+  async upload({ files }: IFilesServiceUpload): Promise<string[]> {
+    console.log(files);
+
+    const waitedFiles = await Promise.all(files);
+    console.log(waitedFiles); // [File, File]
+
+    // 1. 파일을 클라우드 스토리지에 저장하는 로직
+
+    // 1-1) 스토리지 셋팅하기
+    const bucket = '버킷이름입력';
+    const storage = new Storage({
+      projectId: '프로젝트ID입력',
+      keyFilename: '키파일이름입력',
+    }).bucket(bucket);
+
+    // 1-2) 스토리지에 파일 올리기
+    const results = await Promise.all(
+      waitedFiles.map(
+        (el) =>
+          new Promise<string>((resolve, reject) => {
+            el.createReadStream()
+              .pipe(storage.file(el.filename).createWriteStream())
+              .on('finish', () => resolve(`${bucket}/${el.filename}`))
+              .on('error', () => reject('실패'));
+          }),
+      ),
+    );
+    console.log('파일 전송이 완료되었습니다.');
+
+    return results;
+  }
+}
